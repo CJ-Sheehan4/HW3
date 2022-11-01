3c86913632fab6cf4ef4cf35431bede1bb2b2f2f
CJ Sheehan <SheehanCorn@gmail.com>
2022-11-01 18:56:39 -0400

    fixed table.remove()

--- js/myScript.js
+++ js/myScript.js
@@ -1,34 +1,31 @@
 function getInputValues(){
+  // get all input from browser and parse into an int data type
   var minCol = parseInt(document.getElementById("minc").value);
   var maxCol = parseInt(document.getElementById("maxc").value);
   var minRow = parseInt(document.getElementById("minr").value);
   var maxRow = parseInt(document.getElementById("maxr").value);
-  
-  // var table = document.getElementById("table");
-  // table.remove();
-  // var tableParentDiv = document.getElementById("coltable");
-  // var newTable = document.createElement("table");
-  // newTable.id = "table";
-  // tableParentDiv.appendChild(newTable);
-
-  // var newTr = document.createElement("tr");
-  // newTable.appendChild(newTr);
-  // var newTh = document.createElement("th");
-  // newTr.appendChild(newTh);
-  // newTr.appendChild(newTh);
-
-//
-//
-// make a new tbody element and append it to table 
-//
-//
-//
-
+  // remove previous table
+  var table = document.getElementById("table");
+  table.remove();
+  // creates new HTML table
+  // structure: div -> table -> tbody -> tr -> th th 
+  var tableParentDiv = document.getElementById("coltable");
+  var newTable = document.createElement("table");
+  newTable.id = "table";
+  tableParentDiv.appendChild(newTable);
+  var newTbody = document.createElement("tbody");
+  newTable.appendChild(newTbody);
+  var newTr = document.createElement("tr");
+  newTbody.appendChild(newTr);
+  var newTh = document.createElement("th");
+  newTr.appendChild(newTh);
+  var newTh = document.createElement("th");
+  newTr.appendChild(newTh);
+  // initialize elements and populate first column header
   var tbodyElement = document.getElementById("table").firstElementChild;
   var trCollection = tbodyElement.children;
   var tableColHeaders = trCollection[0].children;
   tableColHeaders[1].innerHTML = minCol;
-
   // populating the top row/column headers
   for(var i = minCol + 1; i <= maxCol; i++) {
     var newTh = document.createElement("th");
@@ -55,8 +52,6 @@ function getInputValues(){
     }
   }
 }
-
-
+getInputValues();
+// When button is clicked, calls the function to populate the table
 document.getElementById("button").addEventListener("click", getInputValues);
-
-
